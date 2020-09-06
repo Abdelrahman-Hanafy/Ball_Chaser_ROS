1599c65ee39d6bd6ad719e3ba37d5417a4a8008e
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2020-09-06 10:36:39 +0200

    building the workspace to launch the project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/hanafy/RSEdegree/scat_ws/src
+# Build directory: /home/hanafy/RSEdegree/scat_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ball_chaser")
+subdirs("my_robot")
